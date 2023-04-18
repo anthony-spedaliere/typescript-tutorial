b60d35db6f7bc6a3629e81cc3765aa2e91db5a4c
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-18 15:41:53 -0700

    finished section on type alias and union

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,65 @@
-// initialize variables practice
-let hello: string = "world";
-hello = "test";
+// // initialize variables practice
+// let hello: string = "world";
+// hello = "test";
 
-// practice functions
-const getFullName = (name:string, surname:string): string => {
-    return name + ' ' + surname;
-}
+// // practice functions
+// const getFullName = (name:string, surname:string): string => {
+//     return name + ' ' + surname;
+// }
 
-console.log(getFullName('Chris', 'Farley'));
+// console.log(getFullName('Chris', 'Farley'));
 
-// creating objects
-const user:{name: string, age: number} = {
-    name: 'Larry',
-    age: 56,
-};
+// // creating objects
+// const user:{name: string, age: number} = {
+//     name: 'Larry',
+//     age: 56,
+// };
 
-const user2:{name: string, age: number} = {
-    name: "Jack",
-    age: 25,
-};
+// const user2:{name: string, age: number} = {
+//     name: "Jack",
+//     age: 25,
+// };
 
-// typescript interfaces (entities)
-interface UserInterface {
-    name: string;
-    age?: number;
-    getMessage(): string;
-}
+// // typescript interfaces (entities)
+// interface UserInterface {
+//     name: string;
+//     age?: number;
+//     getMessage(): string;
+// }
 
-// creating a user by interface
-const user3: UserInterface = {
-    name: 'Bob',
-    age: 45,
-    getMessage() {
-        return "Hello " + name;
-    },
-}
+// // creating a user by interface
+// const user3: UserInterface = {
+//     name: 'Bob',
+//     age: 45,
+//     getMessage() {
+//         return "Hello " + name;
+//     },
+// }
 
+// // Union and Type alias in Typescript
+
+// // alias
+// type ID = string;
+// type PopularTag = string;
+// // combining type alias and union
+// type MaybePopularTag = PopularTag | null;
+
+// // interface
+// interface UserInterface {
+//     id: ID;
+//     name: string;
+//     surname: string;
+// }
+
+// const dragonTag: MaybePopularTag = 'dragon';
+
+// const popularTags:PopularTag[] = ['dragon', 'coffee'];
+
+// let username: string = "alex";
+// // '|' is a union operator
+// let pageName: string | number = "1";
+
+// let errorMessage: string | null = null;
+
+// let user: UserInterface | null = null;
 
